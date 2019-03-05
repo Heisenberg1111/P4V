db4467c4b2d7744aa30a72a9962b4ce58fc1f4a9
Franlon <franlon@debian-Pan.PlusUltra>
2019-03-05 15:12:34 +0800

    Add User Input

--- vi_test.py
+++ vi_test.py
@@ -1,8 +1,19 @@
 import time
 
+name="Pan"
+
 x=0
-while x<11:
- print("Hello World")
- time.sleep(1)
- x+=1
+name=input("What is your Name: ")
+
+if name=="Pan":
+	while x<11:
+	 print("Hello Pan")
+	 time.sleep(1)
+	 x+=1
+else:
+
+	while x<11:
+ 	 print("Hello " + name)
+ 	 time.sleep(1)
+ 	 x+=1
 
