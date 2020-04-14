5bd6f3a6d7234ebcebe88dd0bf1b7dc4d0394a99
Franlon <franlon@debian-Pan.PlusUltra>
2020-04-14 10:52:26 +0800

    Enter Values only, No Evaluation if entered value is correct

---
+++ python_institute/hypotenuse.py
@@ -0,0 +1,13 @@
+#Compute for the Hypotenuse
+#
+#c**2=a**2+b**2 (a and b is always positive)
+#
+# Rev1: Enter Values only, no evaluation if entered values
+#	is correct or not. 
+
+legA=float(input("Enter Leg A length: "))
+legB=float(input("Enter Leg B length: "))
+
+hypo=(legA**2+legB**2)**0.5
+
+print("Hypotenuse is equal to",hypo)
