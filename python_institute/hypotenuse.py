f68f1c3daf7879f825f388542331f2f1c8904eb6
Franlon <franlon@debian-Pan.PlusUltra>
2020-04-14 11:38:54 +0800

    Added int to string convertion for var hypo

--- python_institute/hypotenuse.py
+++ python_institute/hypotenuse.py
@@ -3,11 +3,14 @@
 #c**2=a**2+b**2 (a and b is always positive)
 #
 # Rev1: Enter Values only, no evaluation if entered values
-#	is correct or not. 
+#	is correct or not.
+#
+# Rev2: Print the hypotenuse by changing it to str then using
+#	using the concatenation string operatore (+) 
 
 legA=float(input("Enter Leg A length: "))
 legB=float(input("Enter Leg B length: "))
 
-hypo=(legA**2+legB**2)**0.5
+hypo=round((legA**2+legB**2)**0.5,2)
 
-print("Hypotenuse is equal to",hypo)
+print("Hypotenuse is equal to "+str(hypo))
